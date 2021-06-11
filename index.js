d55bdf20854fe4e46afb90fbd5a542e304d05961
YonatanWN <52720132+YonatanWN@users.noreply.github.com>
2021-06-10 23:55:09 -0400

    Websockets Setup

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ var port = process.env.PORT || 5000
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
+var turtleClients = {};
 server.listen(port)
+
+
+
 
 console.log("http server listening on %d", port)
 
@@ -17,6 +21,13 @@ console.log("websocket server created")
 wss.on('connection', function connection(ws) {
   console.log('someone connected');
   ws.on('message', function incoming(message) {
+    if(message.includes("CLIENTINFORMATION")){
+     var splitMessage = message.split(" ");
+     if(splitMessage[1] == "TURTLE"){
+       turtleClients[0] = ws;
+       console.log("Webpage has been added to clients");
+     }
+    }
     console.log('received: %s', message);
     ws.send(message);
   });
